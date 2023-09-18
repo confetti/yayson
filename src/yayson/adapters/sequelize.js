b75e922d502e85521e24addd2a211e837a4977ab
David Wheatley <david.wheatley@raileasy.co.uk>
2023-09-18 22:29:52 +0100

    fix: allow custom sequelize primary key fields

--- src/yayson/adapters/sequelize.js
+++ src/yayson/adapters/sequelize.js
@@ -6,6 +6,31 @@ class SequelizeAdapter extends Adapter {
       return model.get(key)
     }
   }
+
+  static id(model) {
+    // Retain backwards compatibility with older sequelize versions
+    const pkFields =
+      model.constructor && model.constructor.primaryKeys ?
+        Object.keys(model.constructor.primaryKeys) :
+        ['id']
+
+    if (pkFields.length > 1) {
+      throw new Error(
+        'YAYSON does not support Sequelize models with composite primary keys. You can only use one column for your primary key. Currently using: ' +
+          pkFields.join(',')
+      )
+    } else if (pkFields.length < 1) {
+      throw new Error(
+        'YAYSON can only serialize Sequelize models which have a primary key. This is used for the JSON:API model id.'
+      )
+    }
+
+    const id = this.get(model, pkFields[0])
+    if (id === undefined) {
+      return id
+    }
+    return `${id}`
+  }
 }
 
 module.exports = SequelizeAdapter
